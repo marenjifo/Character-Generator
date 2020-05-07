f2f9d189063ddd2ef4dbe0fd56101b2d6975e368
Maria Renjifo <mariarenjifo@iMac-de-Maria.local>
2020-05-06 23:07:36 -0500

    Updates

---
+++ precache-manifest.92e03ac316e4650b98ffa584a20138a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dca3702a3e5200384c8e361c68f633d",
+    "url": "/Character-Generator/index.html"
+  },
+  {
+    "revision": "9ee587cdce9dd22aeec1",
+    "url": "/Character-Generator/static/css/main.cd2969b6.chunk.css"
+  },
+  {
+    "revision": "a5f7f69b880db5582beb",
+    "url": "/Character-Generator/static/js/2.85dc3f62.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Character-Generator/static/js/2.85dc3f62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ee587cdce9dd22aeec1",
+    "url": "/Character-Generator/static/js/main.10d31cd7.chunk.js"
+  },
+  {
+    "revision": "68f7fe29267ddeaf3711",
+    "url": "/Character-Generator/static/js/runtime-main.8406e45f.js"
+  }
+]);
